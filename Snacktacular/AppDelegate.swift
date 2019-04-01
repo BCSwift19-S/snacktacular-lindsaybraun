cb7722004c530d323c7daef57b8e0f20d486dcb4
Lindsay Braun <lindsaybraun@lindsays-macbook-pro.local>
2019-04-01 12:24:58 -0400

    9.4 Completed

--- Snacktacular/AppDelegate.swift
+++ Snacktacular/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+        GMSPlacesClient.provideAPIKey("AIzaSyADjuh8lmt8VAgsQ9U6OVPhAOZHBDm6YTw")
         FirebaseApp.configure()
         return true
     }
